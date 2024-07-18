2d0eb690516c574432bd47a10db1c4862ee5c7af
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2024-07-18 23:12:27 +0100

    fix quotes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,10 +25,10 @@
 
         <Card
             title="More Information"
-            content="<h2 id="about">More information</h2>
+            content="<h2 id='about'>More information</h2>
             <p>
                 More details can be found in our paper <a
-                    href="https://arxiv.org/abs/2407.12707"
+                    href='https://arxiv.org/abs/2407.12707'
                     ><em>TTSDS -- Text-to-Speech Distribution Score</em></a
                 >.
             </p>
@@ -36,14 +36,14 @@
             <h2>Reproducibility</h2>
             <p>
                 To reproduce our results, check out our repository <a
-                    href="https://github.com/ttsds/ttsds">here</a
+                    href='https://github.com/ttsds/ttsds'>here</a
                 >.
             </p>
 
             <h2>Credits</h2>
             <p>
                 This benchmark is inspired by <a
-                    href="https://huggingface.co/spaces/TTS-AGI/TTS-Arena"
+                    href='https://huggingface.co/spaces/TTS-AGI/TTS-Arena'
                     >TTS Arena</a
                 > which instead focuses on the subjective evaluation of TTS models. Our
                 benchmark would not be possible without the many open-source TTS models
@@ -52,56 +52,55 @@
             </p>
             <ul>
                 <li>
-                    <a href="https://keithito.com/LJ-Speech-Dataset/h">LJSpeech</a>
+                    <a href='https://keithito.com/LJ-Speech-Dataset/'>LJSpeech</a>
                 </li>
-                <li><a href="https://www.openslr.org/60/">LibriTTS</a></li>
+                <li><a href='https://www.openslr.org/60/'>LibriTTS</a></li>
                 <li>
-                    <a href="https://datashare.ed.ac.uk/handle/10283/2950">VCTK</a>
+                    <a href='https://datashare.ed.ac.uk/handle/10283/2950'>VCTK</a>
                 </li>
-                <li><a href="https://commonvoice.mozilla.org/">Common Voice</a></li>
-                <li><a href="https://github.com/karolpiczak/ESC-50">ESC-50</a></li>
+                <li><a href='https://commonvoice.mozilla.org/'>Common Voice</a></li>
+                <li><a href='https://github.com/karolpiczak/ESC-50'>ESC-50</a></li>
             </ul>
             <p>And the following metrics/representations/tools:</p>
             <ul>
-                <li><a href="https://arxiv.org/abs/2006.11477">Wav2Vec2</a></li>
-                <li><a href="https://arxiv.org/abs/2006.11477">Hubert</a></li>
-                <li><a href="https://arxiv.org/abs/2110.13900">WavLM</a></li>
+                <li><a href='https://arxiv.org/abs/2006.11477'>Wav2Vec2</a></li>
+                <li><a href='https://arxiv.org/abs/2006.11477'>Hubert</a></li>
+                <li><a href='https://arxiv.org/abs/2110.13900'>WavLM</a></li>
                 <li>
                     <a
-                        href="https://en.wikipedia.org/wiki/Perceptual_Evaluation_of_Speech_Quality"
+                        href='https://en.wikipedia.org/wiki/Perceptual_Evaluation_of_Speech_Quality'
                         >PESQ</a
                     >
                 </li>
-                <li><a href="https://arxiv.org/abs/2204.05841">VoiceFixer</a></li>
+                <li><a href='https://arxiv.org/abs/2204.05841'>VoiceFixer</a></li>
                 <li>
-                    <a href="https://www.cs.cmu.edu/~robust/Papers/KimSternIS08.pdf"
+                    <a href='https://www.cs.cmu.edu/~robust/Papers/KimSternIS08.pdf'
                         >WADA SNR</a
                     >
                 </li>
-                <li><a href="https://arxiv.org/abs/2212.04356">Whisper</a></li>
+                <li><a href='https://arxiv.org/abs/2212.04356'>Whisper</a></li>
                 <li>
-                    <a href="https://huggingface.co/cdminix/masked_prosody_model"
+                    <a href='https://huggingface.co/cdminix/masked_prosody_model'
                         >Masked Prosody Model</a
                     >
                 </li>
                 <li>
                     <a
-                        href="https://github.com/JeremyCCHsu/Python-Wrapper-for-World-Vocoder"
+                        href='https://github.com/JeremyCCHsu/Python-Wrapper-for-World-Vocoder'
                         >PyWorld</a
                     >
                 </li>
-                <li><a href="https://arxiv.org/abs/2210.17016">WeSpeaker</a></li>
-                <li><a href="https://github.com/yistLin/dvector">D-Vector</a></li>
+                <li><a href='https://arxiv.org/abs/2210.17016'>WeSpeaker</a></li>
+                <li><a href='https://github.com/yistLin/dvector'>D-Vector</a></li>
             </ul>"
         />
 
         <p>
             Authors: Christoph Minixhofer, Ondřej Klejch, and Peter Bell
             <br />
-            University of Edinburgh. 
+            University of Edinburgh.
             <br />
-            Contact (Christoph Minixhofer):
-            firstname.lastname@ed.ac.uk
+            Contact (Christoph Minixhofer): firstname.lastname@ed.ac.uk
         </p>
 
         <img src="edi-logo.png" class="edi" width="200" />
