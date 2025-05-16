ee774cddeecb4de77cc617795fe444ece18f3ca2
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2025-05-16 07:50:11 +0200

    add survey redirect

---
+++ src/routes/survey/+page.svelte
@@ -0,0 +1,10 @@
+<!-- redirect to 
+ https://edinburghinformatics.eu.qualtrics.com/jfe/preview/previewId/eba599a7-3a0d-458c-a21b-3a5c56b7dace/SV_3rU86DTSxfzYn1I?Q_CHL=preview&Q_SurveyVersionID=current
+ using meta redirect
+ -->
+<head>
+    <meta
+        http-equiv="refresh"
+        content="0; url=https://edinburghinformatics.eu.qualtrics.com/jfe/preview/previewId/eba599a7-3a0d-458c-a21b-3a5c56b7dace/SV_3rU86DTSxfzYn1I?Q_CHL=preview&Q_SurveyVersionID=current"
+    />
+</head>
